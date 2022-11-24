570f353deaf6c55d48406e812dfd4856b4905ddd
Darth Jones <andrew.b.jones@gmail.com>
2022-11-24 11:12:47 -0600

    Initial commit

---
+++ src/OOMLSharp/NumericExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace OOMLSharp;
+
+public static class NumericExtensions
+{
+    /// <summary>
+    /// Convert vector3 into an OpenSCAD array string.
+    /// </summary>
+    public static string ToScadString(this Vector3 vector3)
+    {
+        const string formatString = "[{0}, {1}, {2}]";
+        object[] components = { vector3.X, vector3.Y, vector3.Z };
+        return ToStringHelper(formatString, components);
+    }
+
+    /// <summary>
+    /// Convert matrix4X4 into an OpenSCAD array string.
+    /// </summary>
+    public static string ToScadString(this Matrix4x4 matrix4X4)
+    {
+        const string formatString = """
+            [ [{0}, {1}, {2}, {3}],
+              [{4}, {5}, {6}, {7}],
+              [{8}, {9}, {10}, {11}],
+              [{12}, {13}, {14}, {15}] ]
+            """;
+        float[] components =
+        {
+            matrix4X4.M11, matrix4X4.M12, matrix4X4.M13, matrix4X4.M14,
+            matrix4X4.M21, matrix4X4.M22, matrix4X4.M23, matrix4X4.M24,
+            matrix4X4.M31, matrix4X4.M32, matrix4X4.M33, matrix4X4.M34,
+            matrix4X4.M41, matrix4X4.M42, matrix4X4.M43, matrix4X4.M44,
+        };
+        return ToStringHelper(formatString, components);
+    }
+
+    private static string ToStringHelper(string formatString, params object[] args)
+    {
+        return String.Format(CultureInfo.InvariantCulture, formatString, args);
+    }
+}
---
+++ test/OOMLSharp.Test/Utility/UseCultureAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace OOMLSharp.Test.Utility;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class UseCultureAttribute : BeforeAfterTestAttribute
+{
+    private readonly CultureInfo _culture;
+    private CultureInfo _originalCulture;
+    private CultureInfo _originalUICulture;
+
+    public string CultureName { get; set; } = "en-us";
+
+    public UseCultureAttribute(string cultureName)
+    {
+        _originalCulture = CultureInfo.InvariantCulture;
+        _originalUICulture = CultureInfo.InvariantCulture;
+        _culture = CultureInfo.CreateSpecificCulture(CultureName);
+    }
+
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        (_originalCulture, _originalUICulture) = (CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture);
+        (CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture) = (_culture, _culture);
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        (CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture) = (_originalCulture, _originalUICulture);
+    }
+}
